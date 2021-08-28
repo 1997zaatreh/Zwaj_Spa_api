184f797a8b1823affe93f296187f1876d92aecb3
zaatreh <zaatreh1997@gmail.com>
2021-08-28 14:15:44 +0300

    Add project files.

---
+++ Zwaj/Controllers/ValuesController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zwaj.Data;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Zwaj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _Context;
+
+        public ValuesController(DataContext Context)
+        {
+
+            _Context = Context;
+
+        }
+
+
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _Context.Values.ToListAsync();
+
+            return Ok(values);
+         
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value =await _Context.Values.FirstOrDefaultAsync(x=>x.id==id);
+            return Ok(value);
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
